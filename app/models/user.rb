c51bd496efc592a225546a7c99ee28570c977218
”Hirogon” <Hirogon>
2019-02-14 23:24:56 +0900

    Rails05_kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
     validates :name, presence: true
+    validates :name, length: { in: 1..15}
     validates :email, presence: true
-
+    validates :email, format: { with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+    validates :password, length: { in: 8..32}
+    validates :password, format:{with:/(?=.*?[a-z])(?=.*?[0-9])/i}
 
     has_secure_password
 end
