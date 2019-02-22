a8353736348063ce5a56cc7cb0b0a09a46c5f2cd
”Hirogon” <Hirogon>
2019-02-22 23:42:57 +0900

    first herock_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
+   get 'topics/new'
+   get 'sessions/new'
    root 'pages#index'
    get 'pages/help'
 
    resources :users
+   resources :topics
+
+   get 'favorites/index'
+   post '/favorites', to: 'favorites#create'
 
    get    '/login',   to: 'sessions#new'
    post   '/login',   to: 'sessions#create'
